cd66fa8ed2c869d1b4d47b35ec424a55632e1c35
Lzocatelli <luizzocatelli2014@gmail.com>
2026-03-06 13:16:35 -0300

    railway db

--- app/database.py
+++ app/database.py
@@ -3,19 +3,22 @@ import os
 from dotenv import load_dotenv
 
 
-load_dotenv()
+load_dotenv(override=False)
 
 def conectar():
     database_url = os.getenv("DATABASE_URL")
+    
     
     if database_url:
         
         if database_url.startswith("postgres://"):
             database_url = database_url.replace("postgres://", "postgresql://", 1)
         
+        print("Conectando via DATABASE_URL...")
         return psycopg2.connect(database_url)
     
     
+    print("Conectando via variáveis individuais...")
     return psycopg2.connect(
         host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT", "5432"),
